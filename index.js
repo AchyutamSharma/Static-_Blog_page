00d99be27c48350bd39ec5f93efedb44cbe6e05f
Achyutam Sharma <achyutamgrd@gmail.com>
2025-10-31 01:25:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+import express from "express";
+import bodyParser from "body-parser";
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+// show all task 
+const msg = "Copyright";
+app.get("/",(req,res)=>{
+
+  res.render("index.ejs",{blogTitleArr,blogTextArr,msg});
+
+});
+
+app.get("/index",(req,res)=>{
+
+  res.render("index.ejs",{blogTitleArr,blogTextArr,msg});
+});  
+app.get("/about",(req,res)=>{
+  res.render("about.ejs",{msg});
+});
+app.get("/contact",(req,res)=>{
+  const msg = "This page is not working.If your really want to contact go to my Github/Linkedin . ";
+  res.render("contact.ejs",{msg})
+});
+
+// Two array
+const blogTitleArr = [];
+const blogTextArr = [];
+
+app.post("/add",(req,res)=>{
+  const {blogTitle,blogText} = req.body;
+  if(blogTitle.trim() !== "" && blogText.trim() !== ""){
+    blogTitleArr.push(blogTitle.trim());
+    blogTextArr.push(blogText.trim());
+  }
+  res.redirect("/");
+});
+
+// delete task
+app.post("/delete/:index",(req,res)=>{
+  const index = req.params.index;
+  blogTitleArr.splice(index,1);
+  blogTextArr.splice(index,1);
+  res.redirect("/");
+
+});
+
+// update task
+app.post("/update/:index",(req,res)=>{
+  const index = req.params.index;
+  const {updatedTitle , updatedText } = req.body;
+
+  if(updatedTitle.trim() !== "" && updatedText.trim() !== ""){
+    blogTitleArr[index] = updatedTitle.trim();
+    blogTextArr[index] = updatedText.trim();
+  }
+  res.redirect("/");
+});
+
+
+app.listen(port, ()=>{
+  console.log(`Listening on port ${port}`);  
+});
+
